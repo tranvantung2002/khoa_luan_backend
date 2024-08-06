4c014a6e7b31bb2639ad9e3db4308e24424a1fac
hoanganh23 <hoanganhnth2k3@gmail.com>
2024-08-07 01:02:15 +0700

    update api company

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 import mysql from "mysql2/promise";
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("findjob","root","Tung_2002@",{
-  host: "14.225.210.106",
+const sequelize = new Sequelize("findjob","root","1832003",{
+  host: "127.0.0.1",
   dialect: "mysql",
-  port: 3306,
+  port: 3307,
 
 })
 
